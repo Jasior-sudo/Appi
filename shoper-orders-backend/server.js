e0427a4badd141bd43c371ecf8891426c4467e69
Jasior-sudo <radekjasior0@gmail.com>
2025-03-05 22:17:37 +0100

    Update server.js

--- shoper-orders-backend/server.js
+++ shoper-orders-backend/server.js
@@ -152,7 +152,7 @@ app.post('/api/webhook/orders', async (req, res) => {
 });
 
 // Uruchomienie serwera
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`🚀 Serwer działa na porcie ${PORT}`);
 });
